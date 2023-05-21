dff3f1ad9205f55aa2c46e44d408cc1d9921d9e4
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2023-05-21 20:06:54 +0300

    add config file

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,10 @@
-console.log('Mesa Mesa');
+import * as express from 'express';
+import {PORT} from './utils/config'; 
+
+const app = express();
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
